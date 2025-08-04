6bba1c220536daf97f76e57078c1a54f53f42777
MariamSamirKeddees <marriiaam11@gmail.com>
2025-08-05 00:55:36 +0300

    adding fe,be,db layers

--- main.tf
+++ main.tf
@@ -302,55 +302,219 @@ resource "aws_key_pair" "mariam_key" {
   public_key = file("~/.ssh/terraform-key.pub")  
 }
 
-resource "aws_launch_template" "mariam-launchtemp" {
-  name          = "mariam-launchtemp-IaC"
+#resource "aws_launch_template" "mariam-launchtemp" {
+#  name          = "mariam-launchtemp-IaC"
+#  image_id      = data.aws_ami.ubuntu.id
+#  instance_type = "t3.micro"
+#  key_name      = aws_key_pair.mariam_key.key_name
+#  
+#  network_interfaces {
+#    associate_public_ip_address = false
+#    security_groups             = [aws_security_group.mariam-sg-ec2.id]
+#  }
+#
+#
+#  user_data = base64encode(<<-EOF
+#              #!/bin/bash
+#              apt update -y
+#              apt install -y httpd
+#              systemctl start httpd
+#              systemctl enable httpd
+#              echo "<h1>Hello from Mariam's ASG</h1>" > /var/www/html/index.html
+#              EOF
+#             )
+#
+#  tag_specifications {
+#    resource_type = "instance"
+#    tags = {
+#      Name = "mariam-ec2-IaC"
+#    }
+#  }
+#}
+#
+#resource "aws_autoscaling_group" "mariam-asg" {
+#  name                = "mariam-asg-IaC"
+#  desired_capacity    = 2
+#  max_size            = 6
+#  min_size            = 2
+#  vpc_zone_identifier = [for k, v in aws_subnet.mariam-subnets : v.id if contains(["mariam-priv-fe-1a", "mariam-priv-fe-1b"], k)]
+#  
+#  target_group_arns = [aws_lb_target_group.mariam-tg.arn]
+#
+#  launch_template {
+#    id      = aws_launch_template.mariam-launchtemp.id
+#    version = "$Latest"
+#  }
+#
+#  tag {
+#    key                 = "Name"
+#    value               = "mariam-asg-IaC"
+#    propagate_at_launch = true
+#  }
+#}
+
+
+###############################################################
+
+resource "aws_launch_template" "lt_fe" {
+  name          = "mariam-lt-fe-IaC"
   image_id      = data.aws_ami.ubuntu.id
   instance_type = "t3.micro"
   key_name      = aws_key_pair.mariam_key.key_name
-  
+
   network_interfaces {
     associate_public_ip_address = false
-    security_groups             = [aws_security_group.mariam-sg-ec2.id]
+    security_groups             = [aws_security_group.mariam-sg-fe.id]
   }
-
 
   user_data = base64encode(<<-EOF
               #!/bin/bash
               apt update -y
-              apt install -y httpd
-              systemctl start httpd
-              systemctl enable httpd
-              echo "<h1>Hello from Mariam's ASG</h1>" > /var/www/html/index.html
+              apt install -y nginx
+              echo "<h1>Frontend - Nginx running BY MARIAM </h1>" > /var/www/html/index.html
+              systemctl enable nginx
+              systemctl start nginx
               EOF
-             )
+  )
 
   tag_specifications {
     resource_type = "instance"
     tags = {
-      Name = "mariam-ec2-IaC"
+      Name = "mariam-fe-ec2-IaC"
     }
   }
 }
 
-resource "aws_autoscaling_group" "mariam-asg" {
-  name                = "mariam-asg-IaC"
-  desired_capacity    = 2
-  max_size            = 6
+resource "aws_autoscaling_group" "asg_fe" {
+  name                = "mariam-asg-fe-IaC"
   min_size            = 2
+  max_size            = 4
+  desired_capacity    = 2
   vpc_zone_identifier = [for k, v in aws_subnet.mariam-subnets : v.id if contains(["mariam-priv-fe-1a", "mariam-priv-fe-1b"], k)]
-  
-  target_group_arns = [aws_lb_target_group.mariam-tg.arn]
+  target_group_arns   = [aws_lb_target_group.mariam-tg.arn]
 
   launch_template {
-    id      = aws_launch_template.mariam-launchtemp.id
+    id      = aws_launch_template.lt_fe.id
     version = "$Latest"
   }
 
   tag {
     key                 = "Name"
-    value               = "mariam-asg-IaC"
+    value               = "fe-instance"
     propagate_at_launch = true
   }
 }
 
+
+
+resource "aws_launch_template" "lt_be" {
+  name          = "mariam-lt-be-IaC"
+  image_id      = data.aws_ami.ubuntu.id
+  instance_type = "t3.micro"
+  key_name      = aws_key_pair.mariam_key.key_name
+
+  network_interfaces {
+    associate_public_ip_address = false
+    security_groups             = [aws_security_group.mariam-sg-be.id]
+  }
+
+  user_data = base64encode(<<-EOF
+              #!/bin/bash
+              apt update -y
+              apt install -y python3-pip
+              pip3 install flask mysql-connector-python
+
+              cat > /home/ubuntu/app.py <<EOL
+              from flask import Flask
+              app = Flask(__name__)
+
+              @app.route("/")
+              def hello():
+                  return "Hello from Backend Flask App! BY MARIAM"
+
+              if __name__ == "__main__":
+                  app.run(host="0.0.0.0", port=443)
+              EOL
+
+              nohup python3 /home/ubuntu/app.py &
+              EOF
+  )
+
+  tag_specifications {
+    resource_type = "instance"
+    tags = {
+      Name = "mariam-be-ec2-IaC"
+    }
+  }
+}
+
+resource "aws_autoscaling_group" "asg_be" {
+  name                = "mariam-asg-be-IaC"
+  min_size            = 2
+  max_size            = 4
+  desired_capacity    = 2
+  vpc_zone_identifier = [for k, v in aws_subnet.mariam-subnets : v.id if contains(["mariam-priv-be-1a", "mariam-priv-be-1b"], k)]
+
+  launch_template {
+    id      = aws_launch_template.lt_be.id
+    version = "$Latest"
+  }
+
+  tag {
+    key                 = "Name"
+    value               = "be-instance"
+    propagate_at_launch = true
+  }
+}
+
+
+resource "random_password" "rds_password" {
+  length  = 16
+  special = true
+}
+
+resource "aws_secretsmanager_secret" "rds_secret" {
+  name = "mariam-rds-secret-IaC"
+}
+
+resource "aws_secretsmanager_secret_version" "rds_secret_version" {
+  secret_id     = aws_secretsmanager_secret.rds_secret.id
+  secret_string = jsonencode({
+    username = "dbadmin"
+    password = random_password.rds_password.result
+  })
+}
+
+resource "aws_db_subnet_group" "rds_subnet_group" {
+  name       = "mariam-rds-subnet-group"
+  subnet_ids = [
+    aws_subnet.mariam-subnets["mariam-priv-db-1a"].id,
+    aws_subnet.mariam-subnets["mariam-priv-db-1b"].id
+  ]
+
+  tags = {
+    Name = "mariam-rds-subnet-group"
+  }
+}
+
+resource "aws_db_instance" "rds_mysql" {
+  identifier              = "mariam-db-iac"
+  engine                  = "mysql"
+  engine_version          = "8.0"
+  instance_class          = "db.t3.micro"
+  allocated_storage       = 20
+  db_subnet_group_name    = aws_db_subnet_group.rds_subnet_group.name
+  vpc_security_group_ids  = [aws_security_group.mariam-sg-db.id]
+  multi_az                = true
+  username                = jsondecode(aws_secretsmanager_secret_version.rds_secret_version.secret_string)["username"]
+  password                = jsondecode(aws_secretsmanager_secret_version.rds_secret_version.secret_string)["password"]
+  skip_final_snapshot     = true
+  publicly_accessible     = false
+
+  tags = {
+    Name = "mariam-mysql-db"
+  }
+
+  depends_on = [aws_db_subnet_group.rds_subnet_group]
+}
 
