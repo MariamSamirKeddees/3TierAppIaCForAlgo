a5a462b183a8fc69ac7447d9c0ca998fae022f60
MariamSamirKeddees <marriiaam11@gmail.com>
2025-08-05 01:18:22 +0300

    adding load balancer listener

--- main.tf
+++ main.tf
@@ -110,6 +110,18 @@ resource "aws_security_group" "mariam-sg-alb" {
   }
 }
 
+resource "aws_lb_listener" "http_listener" {
+  load_balancer_arn = aws_lb.mariam-alb.arn
+  port              = 80
+  protocol          = "HTTP"
+
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.mariam-tg.arn
+  }
+}
+
+
 resource "aws_vpc_security_group_ingress_rule" "port-443-inbound" {
   security_group_id = aws_security_group.mariam-sg-alb.id
   cidr_ipv4         = aws_vpc.mariam-3tierApp-IaC.cidr_block
