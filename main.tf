18616e67ab1cfdaaab332be870e88d53458c9bac
MariamSamirKeddees <marriiaam11@gmail.com>
2025-08-05 12:25:57 +0300

    adding bastion, and edit some alb relating code

--- main.tf
+++ main.tf
@@ -105,6 +105,28 @@ resource "aws_security_group" "mariam-sg-alb" {
   description = "Allow HTTP and HTTPS inbound traffic and all outbound traffic"
   vpc_id      = aws_vpc.mariam-3tierApp-IaC.id
 
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
   tags = {
     Name = "mariam-sg-alb-IaC"
   }
@@ -144,8 +166,46 @@ resource "aws_security_group" "mariam-sg-fe" {
   description = "Allow traffic from ALB only"
   vpc_id      = aws_vpc.mariam-3tierApp-IaC.id
 
+
+  ingress {
+    from_port       = 80
+    to_port         = 80
+    protocol        = "tcp"
+    security_groups = [aws_security_group.mariam-sg-alb.id]
+  }
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["10.0.0.0/16"] 
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+
   tags = {
     Name = "mariam-sg-fe-IaC"
+  }
+}
+
+resource "aws_instance" "bastion" {
+  ami           = data.aws_ami.ubuntu.id
+  instance_type = "t3.micro"
+  key_name      = aws_key_pair.mariam_key.key_name
+  subnet_id     = aws_subnet.mariam-subnets["mariam-pub-1a"].id
+  
+  vpc_security_group_ids = [
+    aws_security_group.mariam-sg-ec2.id
+  ]
+
+  tags = {
+    Name = "mariam-bastion-IaC"
   }
 }
 
