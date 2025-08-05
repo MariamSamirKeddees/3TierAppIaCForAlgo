8cdba01648e3313a5e596d565d4df7b0c164e2a6
MariamSamirKeddees <marriiaam11@gmail.com>
2025-08-05 11:56:32 +0300

    replace alb-to-fe port from 443 to 80

--- main.tf
+++ main.tf
@@ -151,12 +151,12 @@ resource "aws_security_group" "mariam-sg-fe" {
 
 resource "aws_security_group_rule" "alb-to-fe" {
   type                     = "ingress"
-  from_port                = 443
-  to_port                  = 443
+  from_port                = 80
+  to_port                  = 80
   protocol                 = "tcp"
   source_security_group_id = aws_security_group.mariam-sg-alb.id
   security_group_id        = aws_security_group.mariam-sg-fe.id
-  description              = "Allow HTTPS traffic from source_sg"
+  description              = "Allow HTTP traffic from source_sg"
 }
 
 resource "aws_security_group" "mariam-sg-be" {
@@ -223,7 +223,6 @@ locals {
     if startswith(key, "mariam-priv")
   }
 
-  
 }
 
 resource "aws_lb" "mariam-alb" {
